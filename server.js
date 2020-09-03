f2c1716809b27396ed0f93953f1cbddfc441b937
julienmalard <julien.malard@mail.mcgill.ca>
2020-09-03 12:03:37 -0400

    ஹெரோக்குக்காக

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.use(serveStatic(path.join(__dirname, "/dist")))
 var history = require('connect-history-api-fallback')
 app.use(history())
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
